c0e8c249a5a74650288c0ae8ef54f49365378617
Jorge Pardiñas <jparbros@gmail.com>
2012-04-10 19:41:07 -0500

    nueva manera de cargar las imagenes en los seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,15 +86,15 @@ Admin.create(
 #
 
 banner_bc_001 = Banner.create!(active: true, site_id: baja_california_site.id)
-banner_bc_001.image = File.read(File.join(Rails.root, '/db/images/bc001.gif'))
+banner_bc_001.image = File.open(File.join(Rails.root, '/db/images/bc001.gif'))
 banner_bc_001.save
 
 banner_bc_002 = Banner.create!(active: true, site_id: baja_california_site.id)
-banner_bc_002.image = File.read(File.join(Rails.root, '/db/images/bc002.gif'))
+banner_bc_002.image = File.open(File.join(Rails.root, '/db/images/bc002.gif'))
 banner_bc_002.save
 
 banner_bc_003 = Banner.create!(active: true, site_id: baja_california_site.id)
-banner_bc_003.image = File.read(File.join(Rails.root, '/db/images/bc003.gif'))
+banner_bc_003.image = File.open(File.join(Rails.root, '/db/images/bc003.gif'))
 banner_bc_003.save
 
 
@@ -103,15 +103,15 @@ banner_bc_003.save
 #
 
 banner_ch_001 = Banner.create!(active: true, site_id: chihuahua_site.id)
-banner_ch_001.image = File.read(File.join(Rails.root, '/db/images/ch001.gif'))
+banner_ch_001.image = File.open(File.join(Rails.root, '/db/images/ch001.gif'))
 banner_ch_001.save
 
 banner_ch_002 = Banner.create!(active: true, site_id: chihuahua_site.id)
-banner_ch_002.image = File.read(File.join(Rails.root, '/db/images/ch002.gif'))
+banner_ch_002.image = File.open(File.join(Rails.root, '/db/images/ch002.gif'))
 banner_ch_002.save
 
 banner_ch_003 = Banner.create!(active: true, site_id: chihuahua_site.id)
-banner_ch_003.image = File.read(File.join(Rails.root, '/db/images/ch003.gif'))
+banner_ch_003.image = File.open(File.join(Rails.root, '/db/images/ch003.gif'))
 banner_ch_003.save
 
 #
@@ -119,15 +119,15 @@ banner_ch_003.save
 #
 
 banner_nl_001 = Banner.create!(active: true, site_id: nuevo_leon_site.id)
-banner_nl_001.image = File.read(File.join(Rails.root, '/db/images/nl001.gif'))
+banner_nl_001.image = File.open(File.join(Rails.root, '/db/images/nl001.gif'))
 banner_nl_001.save
 
 banner_nl_002 = Banner.create!(active: true, site_id: nuevo_leon_site.id)
-banner_nl_002.image = File.read(File.join(Rails.root, '/db/images/nl002.gif'))
+banner_nl_002.image = File.open(File.join(Rails.root, '/db/images/nl002.gif'))
 banner_nl_002.save
 
 banner_nl_003 = Banner.create!(active: true, site_id: nuevo_leon_site.id)
-banner_nl_003.image = File.read(File.join(Rails.root, '/db/images/nl003.gif'))
+banner_nl_003.image = File.open(File.join(Rails.root, '/db/images/nl003.gif'))
 banner_nl_003.save
 
 #
