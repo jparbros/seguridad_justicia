e3f7dcf319feaf688d009bd8214cef10488250cc
Jorge Pardiñas <jparbros@gmail.com>
2012-04-19 09:43:00 -0500

    removing event calendar gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 SeguridadJusticia::Application.routes.draw do
 
-  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
-
-  get "debates/index"
-
-  get "debates/new"
-
-  get "debates/edit"
-
-  get "debates/_form"
-
   devise_for :admins, :controllers => { :sessions => "admin/sessions" }
 
   namespace :admin do
