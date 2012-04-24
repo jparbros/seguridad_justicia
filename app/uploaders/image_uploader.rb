bc52202cae267d885cc40cdea6124b8684715e33
Jorge Pardiñas <jparbros@gmail.com>
2012-04-24 10:12:25 -0500

    barra principal del home

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [100, 26]
-  process :resize_to_fill => [605, 155]
+  process :resize_to_fill => [652, 155]
 
   #
   # def scale(width, height)
