6c4de707e2722c5cb7dccf7a669c0a35c969a49b
Jorge Pardiñas <jparbros@gmail.com>
2012-05-03 14:10:03 -0500

    google analytics code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
       'CHSegurayJusta'
    end
   end
+
+  def ga_code
+    if @site.hostname.match /bc/
+      'UA-31401146-1'
+    elsif @site.hostname.match /nl/
+      'UA-31401146-2'
+    elsif @site.hostname.match /ch/
+      'UA-31401146-3'
+   end
+  end
 end
