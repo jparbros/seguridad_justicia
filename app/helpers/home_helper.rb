addaf5931cde39f1fbaade77fb121c7ab3ad238f
Jorge Pardiñas <jparbros@gmail.com>
2012-05-11 15:35:23 -0500

    Cambios en las frases y colores

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def question_phrase
+    if @site.hostname.match /nl/
+      "#{@site.label} ser&iacute;a m&aacute;s justo y seguro si:"
+    else
+      "#{@site.label} ser&iacute;a m&aacute;s justa y segura si:"
+    end
+  end
 end
