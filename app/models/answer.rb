553df93b247ba8a8a48f67676c531bb891bcfbd2
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-25 19:05:20 -0500

    Arreglando error en el video de youtube y en la pagina de respuestas en el admin

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,7 +9,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   def url_for_youtube
-    parsed_url = self.url.match(/v=.*&/).to_s.gsub('v=','').gsub('&','')
+    parsed_url = self.url.match(/v=.*/).to_s.gsub('v=','').gsub('&','')
     "http://www.youtube.com/embed/#{parsed_url}"
   end
 end
