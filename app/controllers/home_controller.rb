ff4b7b1b47b71326c62f37835dbaf8d51b4475fc
Jorge Pardiñas <jorge.parbros@crowdint.com>
2012-06-26 11:06:29 -0500

    jalando las preguntas por sitio, en lugar de las mejores

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def show
     @banners = Banner.by_site(@site.id).active
     @recent_questions = Question.to_home(@site.id)
-    @best_questions = Question.best_by_site(@site.id)
+    @best_questions = Question.by_site(@site.id)
   end
 end
