6eb04aa6581c71455b6b49e221dafe6499270ef4
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-10-25 19:32:49 -0500

    diputados federales ya se muestran

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def show
     @banners = Banner.by_site(@site.id).active
-    @diputados_federales = Representative.by_site(@site.id).where(position: 'diputado_fereal')
+    @diputados_federales = Representative.by_site(@site.id).where(position: 'diputado_federal')
     @senadores = Representative.by_site(@site.id).where(position: 'senador')
     @alcaldes = Representative.by_site(@site.id).where(position: 'presidente_municipal')
     @diputados_locales = Representative.by_site(@site.id).where(position: 'diputado_local')
