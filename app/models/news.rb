61fd0eee95d56bd4d8bcc921fc3f5d68002c7053
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-12-03 18:36:36 -0600

    arreglando news en el sidebar

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,4 +4,8 @@ class News < ActiveRecord::Base
   def self.by_site(site_id)
     where(site_id: site_id)
   end
+  
+  def self.to_sidebar(site_id)
+    by_site(site_id).limit(3).order('date DESC')
+  end
 end
