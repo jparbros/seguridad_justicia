2f33d08d844deed7bad1802fd2947e8deb504801
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-07-23 02:19:01 -0500

    mejorando la busqueda por distritos

--- app/controllers/distritos_controller.rb
+++ app/controllers/distritos_controller.rb
@@ -1,8 +1,13 @@
 class DistritosController < ApplicationController
   def show
     @representatives = Representative
-    District.find(params[:id]).sections.each do |section|
-      @representatives =  Representative.by_site(@site.id).where("representatives.section LIKE ? ", "%#{params[:id]}%").group_by {|representative| representative.position}
+    district = District.find_by_district_number(params[:id])
+    if district.present?
+      district.sections.each do |section|
+        @representatives =  Representative.by_site(@site.id).where("representatives.section LIKE ? ", "%#{params[:id]}%").group_by {|representative| representative.position}
+      end 
+    else
+      @representatives = []
     end
   end
   
