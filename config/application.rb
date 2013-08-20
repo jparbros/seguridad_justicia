21f893414d1fff519d701d5e7f36adda36a40108
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-08-20 18:35:17 -0500

    ruby-prof

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,5 @@ module SeguridadJusticia
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
-    config.middleware.use Rack::RubyProf
   end
 end
