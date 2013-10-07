fbd1293b769f299454e283c5e1bd00626780b433
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-10-07 12:17:25 -0500

    cleaning cache

---
+++ app/models/cache_observer.rb
@@ -0,0 +1,7 @@
+class CacheObserver < ActiveRecord::Observer
+   observe :answer, :banner, :candidate, :election, :phrase, :question, :representative, :setting, :vote
+   
+   def after_save(object)
+     Rails.cache.clear
+   end
+end
