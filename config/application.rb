88c73be61d13329c65c0bbac59494f5b88ccbd70
Jorge Pardiñas <jorge.parbros@parbros.com>
2013-10-07 12:54:28 -0500

    calling cache observer

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module SeguridadJusticia
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = :cache
+    config.active_record.observers = :cache_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
